3ffb7c6a6d70a2c995d1ce7565db8f004fc1f3f2
Hao Nghiem <qtvhao@gmail.com>
2025-05-10 17:59:24 +0700

    Add

--- supabase/migrations/20250510175531_make_anon_social_media_accounts.sql
+++ supabase/migrations/20250510175531_make_anon_social_media_accounts.sql
@@ -4,4 +4,6 @@ select account_id
 from public.social_media_account;
 
 -- Enable RLS for origin table
-alter table public.origin enable row level security;
+alter table public.social_media_account enable row level security;
+alter table public.youtube_account_details enable row level security;
+alter table public.tiktok_account_details enable row level security;
