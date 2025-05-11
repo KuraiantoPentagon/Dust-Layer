79bf685ba9143ad0fe48b5a722c38a4f86ab4a40
Hao Nghiem <qtvhao@gmail.com>
2025-05-11 19:33:58 +0700

    Add

--- supabase/migrations/20250511175417_replace_function_create_todo_analytics_job.sql
+++ supabase/migrations/20250511175417_replace_function_create_todo_analytics_job.sql
@@ -28,3 +28,4 @@ BEGIN
   RETURN v_job_id;
 END;
 $$ LANGUAGE plpgsql;
+-- INSERT INTO "public"."cgm_channel_analytics_job" ("id", "social_media_account_id", "scheduled_time", "status", "started_at", "finished_at", "total_videos", "error_log") VALUES ('1162eb8f-5c63-4a60-8d87-dd619956e31e', '5fc1220a-7720-4b5d-b425-9b4a8e8d1f46', '2025-05-11 12:25:39.109037+00', 'Pending', null, null, null, null);
