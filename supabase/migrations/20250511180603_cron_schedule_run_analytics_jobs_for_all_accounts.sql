81cb7d0ee68471d92224168adb4ed6a928c387e2
Hao Nghiem <qtvhao@gmail.com>
2025-05-11 18:23:54 +0700

    Add

--- supabase/migrations/20250511180603_cron_schedule_run_analytics_jobs_for_all_accounts.sql
+++ supabase/migrations/20250511180603_cron_schedule_run_analytics_jobs_for_all_accounts.sql
@@ -12,5 +12,5 @@ $$ LANGUAGE plpgsql;
 SELECT cron.schedule(
   'analytics_jobs_every_5_min',
   '*/5 * * * *',
-  $$CALL public.run_analytics_jobs_for_all_accounts();$$
+  $$PERFORM public.run_analytics_jobs_for_all_accounts();$$
 );
